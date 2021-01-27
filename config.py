ab6f7c2bb1224ce09b4ae55978fc8b88f2c383a0
grendy <grendy@intama.info>
2021-01-27 15:56:26 +0700

    initial commit

---
+++ config.py
@@ -0,0 +1,3 @@
+pin_number ="070197"
+link_produk ="https://shopee.co.id/-VOUCHER-REALSP100-realme-Watch-S-Pro-AMOLED-Screen-GPS-5ATM-Water-Resistant--i.109598751.6975299725"
+cookiee = 'iVfObEzQe03TKxRgl2Vbp9ZDcyIC7o4ZXa3nheNbCtXNvsChrK0BSkn69Gcc9mIJzvzpXQuAte8PMFCypRVL5tWaTANh7v63QR'
